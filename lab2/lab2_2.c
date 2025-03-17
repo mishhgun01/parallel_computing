dd3e0f996bb4f007f92432c98d3431cd0d83ba50
Михаил Голубев <mishhgun@MacBook-Air-Mihail.local>
2025-03-17 18:34:15 +0300

    lab2

--- lab2/lab2_2.c
+++ lab2/lab2_2.c
@@ -1,36 +1,81 @@
-#include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mpi.h"
 
-int main(int argc, char *argv[])
+#define DEFAULT_ARR_SIZE 10
+
+int main(int argc, char **argv)
 {
     int rank, size;
-    int min_positive = INT_MAX;
+    int local_min = INT_MAX;
+    int global_min = INT_MAX;
+    int array_size = 20;
 
-    // Инициализация MPI
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    srand(rank * time(NULL));
-
-    int random_number = rand() % 100 + 1;
-    printf("Процесс %d сгенерировал число: %d\n", rank, random_number);
-
-    if (random_number > 0 && random_number < min_positive)
-    {
-        min_positive = random_number;
-    }
-
-    // Собираем минимальные значения со всех процессов
-    int global_min_positive;
-    MPI_Reduce(&min_positive, &global_min_positive, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0)
     {
-        printf("Минимальный положительный элемент: %d\n", global_min_positive);
+        if (argc > 1)
+        {
+            array_size = atoi(argv[1]);
+            if (array_size <= 0)
+            {
+                printf("Размер массива не задан, используется размер по умолчанию (20).\n");
+                array_size = DEFAULT_ARR_SIZE;
+            }
+        }
+        printf("Используемый размер массива: %d\n", array_size);
     }
 
+    MPI_Bcast(&array_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int *array = NULL;
+    if (rank == 0)
+    {
+        array = (int *)malloc(array_size * sizeof(int));
+        srand(time(NULL));
+        printf("Массив: ");
+        for (int i = 0; i < array_size; ++i)
+        {
+            array[i] = rand() % 100 - 50;
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+    }
+
+    int local_size = array_size / size;
+    int *local_array = (int *)malloc(local_size * sizeof(int));
+
+    MPI_Scatter(array, local_size, MPI_INT, local_array, local_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < local_size; ++i)
+    {
+        if (local_array[i] > 0 && local_array[i] < local_min)
+        {
+            local_min = local_array[i];
+        }
+    }
+
+    MPI_Reduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        if (global_min != INT_MAX)
+        {
+            printf("Минимальный положительный элемент: %d\n", global_min);
+        }
+        else
+        {
+            printf("В массиве нет положительных элементов.\n");
+        }
+        free(array);
+    }
+
+    free(local_array);
     MPI_Finalize();
     return 0;
 }
